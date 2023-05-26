1e71ae113788102320bdf94678b6129d387ece95
copterbuddy <copterbuddy@gmail.com>
2023-05-26 19:31:19 +0700

    extract method CheckCanChangeOnlyCoin3

--- Change/Program.cs
+++ Change/Program.cs
@@ -8,27 +8,34 @@ public class Program
     }
     public static string Change(int amount)
     {
-        int changeCoin3;
-        int changCoin10;
+        int changeCoin3 = 0;
+        int changCoin10 = 0;
+
+        if (CheckCanChangeOnlyCoin3(amount))
+        {
+            changCoin10 = 0;
+            changeCoin3 = amount / 3;
+            return $"{changCoin10} {changeCoin3}";
+        }
 
         changCoin10 = amount / 10;
         int remeaningAmount = changCoin10 * 10;
 
         changeCoin3 = (amount - remeaningAmount) / 3;
 
-        int use3coinonly = amount % 3;
-        if (use3coinonly == 0)
-        {
-            changCoin10 = 0;
-            changeCoin3 = amount / 3;
-        }
+
 
         if ((changeCoin3 * 3) + (changCoin10 * 10) == amount)
         {
             return $"{changCoin10} {changeCoin3}";
-            
+
         }
 
         return "Cannot Change";
+    }
+
+    private static bool CheckCanChangeOnlyCoin3(int amount)
+    {
+        return amount % 3 == 0;
     }
 }
