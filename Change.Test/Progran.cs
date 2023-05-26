76037485fa1d4fc7e5534875c152a376a5c88029
copterbuddy <copterbuddy@gmail.com>
2023-05-26 19:56:59 +0700

    case 13 - 20

--- Change.Test/Progran.cs
+++ Change.Test/Progran.cs
@@ -11,6 +11,8 @@ public class Progran
     [InlineData(7)]
     [InlineData(8)]
     [InlineData(11)]
+    [InlineData(14)]
+    [InlineData(17)]
     public void Want_CannotChange(int paid)
     {
         string want = Program.Change(paid);
@@ -66,5 +68,65 @@ public class Progran
         string want = Program.Change(paid);
 
         Assert.Equal("0 4", want);
+    }
+
+    [Fact]
+    public void Want_1_1_Give_13()
+    {
+        int paid = 13;
+
+        string want = Program.Change(paid);
+
+        Assert.Equal("1 1", want);
+    }
+
+    [Fact]
+    public void Want_0_5_Give_15()
+    {
+        int paid = 15;
+
+        string want = Program.Change(paid);
+
+        Assert.Equal("0 5", want);
+    }
+
+    [Fact]
+    public void Want_1_2_Give_16()
+    {
+        int paid = 16;
+
+        string want = Program.Change(paid);
+
+        Assert.Equal("1 2", want);
+    }
+
+    [Fact]
+    public void Want_0_6_Give_18()
+    {
+        int paid = 18;
+
+        string want = Program.Change(paid);
+
+        Assert.Equal("0 6", want);
+    }
+
+    [Fact]
+    public void Want_1_3_Give_19()
+    {
+        int paid = 19;
+
+        string want = Program.Change(paid);
+
+        Assert.Equal("1 3", want);
+    }
+
+    [Fact]
+    public void Want_2_0_Give_20()
+    {
+        int paid = 20;
+
+        string want = Program.Change(paid);
+
+        Assert.Equal("2 0", want);
     }
 }
