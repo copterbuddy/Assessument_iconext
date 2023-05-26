612e6259327d9a4c00dad4d53b07874c52adf20a
copterbuddy <copterbuddy@gmail.com>
2023-05-26 20:10:42 +0700

    use property from object

--- Change/Program.ChangeModel.cs
+++ Change/Program.ChangeModel.cs
@@ -4,14 +4,49 @@ public partial class Program
 {
     public class ChangeModel
     {
+        public const int COIN3 = 3;
+        public const int COIN10 = 10;
+
         public int Amount { get; set; } = 0;
         public int TotalCoin3 { get; set; } = 0;
         public int TotalCoin10 { get; set; } = 0;
+        public int RemeaningAmount { get; set; } = 0;
         public ChangeModel(int amount)
         {
             Amount = amount;
         }
 
-        public string DisplayResult => $"{TotalCoin10} {TotalCoin3}";
+        public string DisplayChangeResult => $"{TotalCoin10} {TotalCoin3}";
+        public string DisplayCannotChangeResult => "Cannot Change";
+
+        public int GetCoin3FromAmount()
+        {
+            return Amount / COIN3;
+        }
+
+        public int GetCoin10FromAmount()
+        {
+            return Amount / COIN10;
+        }
+
+        public int GetRemeaningAmount()
+        {
+            return TotalCoin10 * COIN10;
+        }
+
+        public int GetCoin3FromRemeaningAmount()
+        {
+            return (Amount - RemeaningAmount) / COIN3;
+        }
+
+        public bool CheckCanChangeOnlyCoin3()
+        {
+            return Amount % COIN3 == 0;
+        }
+
+        public bool CheckCanChange()
+        {
+            return (TotalCoin3 * COIN3) + (TotalCoin10 * COIN10) == Amount;
+        }
     }
 }
--- Change/Program.cs
+++ Change/Program.cs
@@ -2,8 +2,6 @@
 
 public partial class Program
 {
-    public static int COIN3 = 3;
-    public static int COIN10 = 10;
 
     static void Main(string[] args)
     {
@@ -11,56 +9,25 @@ public partial class Program
     }
     public static string Change(int amount)
     {
-
         ChangeModel changeModel = new ChangeModel(amount: amount);
 
-        if (CheckCanChangeOnlyCoin3(amount))
+        if (changeModel.CheckCanChangeOnlyCoin3())
         {
-            changeModel.TotalCoin3 = GetCoin3FromAmount(amount);
-            return changeModel.DisplayResult;
+            changeModel.TotalCoin3 = changeModel.GetCoin3FromAmount();
+            return changeModel.DisplayChangeResult;
         }
 
-        changeModel.TotalCoin10 = GetCoin10FromAmount(amount);
-        int remeaningAmount = GetRemeaningAmount(changeModel.TotalCoin10);
+        changeModel.TotalCoin10 = changeModel.GetCoin10FromAmount();
+        changeModel.RemeaningAmount = changeModel.GetRemeaningAmount();
 
-        changeModel.TotalCoin3 = GetCoin3FromRemeaningAmount(amount, remeaningAmount);
+        changeModel.TotalCoin3 = changeModel.GetCoin3FromRemeaningAmount();
 
-        if (CheckCanChange(changeModel))
+        if (changeModel.CheckCanChange())
         {
-            return changeModel.DisplayResult;
+            return changeModel.DisplayChangeResult;
 
         }
 
-        return "Cannot Change";
-    }
-
-    private static int GetRemeaningAmount(int totalCoin10)
-    {
-        return totalCoin10 * COIN10;
-    }
-
-    private static int GetCoin3FromRemeaningAmount(int amount, int remeaningAmount)
-    {
-        return (amount - remeaningAmount) / COIN3;
-    }
-
-    private static int GetCoin10FromAmount(int amount)
-    {
-        return amount / COIN10;
-    }
-
-    private static int GetCoin3FromAmount(int amount)
-    {
-        return amount / COIN3;
-    }
-
-    private static bool CheckCanChange(ChangeModel canChangeModel)
-    {
-        return (canChangeModel.TotalCoin3 * COIN3) + (canChangeModel.TotalCoin10 * COIN10) == canChangeModel.Amount;
-    }
-
-    private static bool CheckCanChangeOnlyCoin3(int amount)
-    {
-        return amount % COIN3 == 0;
+        return changeModel.DisplayCannotChangeResult;
     }
 }
