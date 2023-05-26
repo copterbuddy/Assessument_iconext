3f463301087e3ed5ebe12f13430ddf55144b8150
copterbuddy <copterbuddy@gmail.com>
2023-05-26 18:21:19 +0700

    init project

---
+++ Change.Test/Progran.cs
@@ -0,0 +1,10 @@
+namespace Change.Test;
+
+public class Progran
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Change/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Change;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
